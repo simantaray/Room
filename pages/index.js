5bcfd991c9087d5d298ff05fab58ac5d7eb0dacd
simantaray <simantaray89@gmail.com>
2022-03-05 20:32:09 +0530

    add : banner

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Banner from '../comps/Banner'
 import Header from '../comps/Header'
 import styles from '../styles/Home.module.css'
 
@@ -13,6 +14,7 @@ export default function Home() {
       {/* header */}
       <Header />
       {/* banner */}
+      <Banner />
 
 
      
